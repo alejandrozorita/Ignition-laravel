2054853e7b649d4f60725acc21a9e47291953660
Alejandro Zorita <alejandro.zorita@medssocial.com>
2020-05-10 21:15:04 +0200

    new exception

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Exceptions\MyCustomException;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
